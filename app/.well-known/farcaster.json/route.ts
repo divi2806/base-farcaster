65a1d416db2ef2c3b0a691bc71a26902ec6c7502
NinjasPyajamas <divyansh242805@gmail.com>
2025-12-07 18:30:52 +0530

    Added manifest

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -6,20 +6,20 @@ export async function GET() {
     "frame": {
       "name": "survivor-zombies",
       "version": "1",
-      "iconUrl": "https://survivor-zombies.vercel.app/images/icon.png",
-      "homeUrl": "https://survivor-zombies.vercel.app",
-      "imageUrl": "https://survivor-zombies.vercel.app/images/image.png",
-      "splashImageUrl": "https://survivor-zombies.vercel.app/images/splash.png",
+      "iconUrl": "https://killz-game-zombie.vercel.app/images/icon.png",
+      "homeUrl": "https://killz-game-zombie.vercel.app",
+      "imageUrl": "https://killz-game-zombie.vercel.app/images/image.png",
+      "splashImageUrl": "https://killz-game-zombie.vercel.app/images/splash.png",
       "splashBackgroundColor": "#111111",
       "webhookUrl": "https://survivor-zombies.vercel.app/api/webhook",
       "subtitle": "survive the zombie apocalypse",
-      "description": "Fight zombies and survive the night",
+      "description": "Fight zombies and survive the night and prevent from getting infected",
       "primaryCategory": "games"
     },
     "accountAssociation": {
-    "header": "eyJmaWQiOjExMDg1NzQsInR5cGUiOiJhdXRoIiwia2V5IjoiMHhkODUzOTVERWYzZDYzM0U3ODYyOTFiZjlERTU0ZDMyNGVlYkM3OTE3In0",
-    "payload": "eyJkb21haW4iOiJqdW1wZmluYWwudmVyY2VsLmFwcCJ9",
-    "signature": "1bA7P3VPHAsGMdMnoxf8SLWHhVoohnfN/c1EoYvT7xo8ttSZ099HrC31BaDDsQPQaxp4ASRabDcBi16A9SWOZxs="
+     "header": "eyJmaWQiOjExNDI1MzksInR5cGUiOiJhdXRoIiwia2V5IjoiMHgyODUyMTE3NzI2ODcyNTY0YThiZDk3M2E5OTIzOGEzOTdiMjgzMUJmIn0",
+    "payload": "eyJkb21haW4iOiJraWxsei1nYW1lLXpvbWJpZS52ZXJjZWwuYXBwIn0",
+    "signature": "MA1uH/+BC23CI38qenJw3gOBKUfqhGvTow5MJZAk4DlSEOIgMHhWCgBoej088rMKoDZEDsuFpMjUkMpuLvudQhw="
   },
    "baseBuilder": {
     "allowedAddresses": ["0x721f07F9E4b5b2D522D0D657cCEebfb64487d8DC"]
